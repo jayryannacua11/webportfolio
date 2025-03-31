8cd7a67a4d753681b2fc21eededeaf29bba6eb2a
Jay Ryan Nacua <jayryannacua11@gmail.com>
2025-04-01 06:12:50 +0800

    Updated Projects

---
+++ js/plugins.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){ 
+    console.log('World')
+   	$(".owl-carousel").owlCarousel({
+		items: 3,
+		nav: false,
+		dots: true,
+		loop: false,
+		margin:10,
+		responsive : {
+		291 : {
+			items:1
+		},
+		801 : {
+			items:2
+		},
+		1025 : {
+			items:4
+		}
+	}
+	});
+
+    console.log('Hello')
+});
